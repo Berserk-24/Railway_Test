eb9e75e3f340b9a881aa7b360841c41cd567475b
Juan Calderon <108417563+Berserk-24@users.noreply.github.com>
2026-04-28 17:34:31 +0000

    esto

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get('/productos', async (req, res) => {
   res.json(result.rows);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
